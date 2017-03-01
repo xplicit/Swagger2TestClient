9299aec36360edd29e9ceb7b547d30f49f6bd356
Sergey Zhukov <svg@ngs.ru>
2017-03-01 19:47:31 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TestClient;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test client=new Test(new Uri("http://localhost:49204/"));
+
+            var res = client.Hello.Get("World");
+
+            Console.WriteLine("res={0}", res.Result);
+        }
+    }
+}
